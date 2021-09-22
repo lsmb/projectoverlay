428b713a52fd1e0bffe9c6720a98cc851eb60a65
holo-rl <miro.p.aho@gmail.com>
2021-09-22 09:45:30 +0300

    Initial

---
+++ ProjectOverlay/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectOverlay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GraphWindow graphWindow = new GraphWindow();
+        public MainWindow()
+        {
+            InitializeComponent();
+            graphWindow.Show();
+            graphWindow.WindowStartupLocation = WindowStartupLocation.Manual;
+            graphWindow.Top = 0;
+            graphWindow.Left = SystemParameters.PrimaryScreenWidth - graphWindow.Width;
+        }
+    }
+}
